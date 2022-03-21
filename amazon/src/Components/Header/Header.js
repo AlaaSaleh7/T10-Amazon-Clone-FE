74773bb610982ad6128d4a81293e5e05687ed52c
AlaaSaleh7 <allaasaleeh7@gmail.com>
2022-03-21 14:02:38 +0300

    task details

---
+++ amazon/src/Components/Header/Header.js
@@ -0,0 +1,48 @@
+import { React, useState } from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+function Header() {
+  const [{ user }] = useState();
+
+  return (
+    <div className="header">
+      <Link to="/">
+        <img className="header-logo" />
+      </Link>
+
+      <div className="header-search">
+        <input className="header-searchInput" type="text" />
+      </div>
+
+      <div className="header-nav">
+        <Link to={!user && "/login"}>
+          <div className="header-option">
+            <span className="header-optionLineOne">
+              Hello {!user ? "Guest" : user.email}
+            </span>
+            <span className="header-optionLineTwo"></span>
+          </div>
+        </Link>
+
+        <Link to="/orders">
+          <div className="header-option">
+            <span className="header-optionLineOne">Returns</span>
+            <span className="header-optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+
+        <div className="header-option">
+          <span>Your</span>
+          <span>Prime</span>
+        </div>
+
+        <Link to="/checkout">
+          <div className="header-optionBasket"></div>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
